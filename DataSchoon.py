d7e63503810e028043f060c7077d4ed8ec15b00d
waweewawee <wilmsakasha@gmail.com>
2023-06-07 12:58:51 +0200

    opschoon script

---
+++ DataSchoon.py
@@ -0,0 +1,86 @@
+import pyodbc
+
+server_name = 'LAPTOP-NQNF412J\\SQLEXPRESS'
+database_name = 'DEDSproject'
+connection_string = f'DRIVER={{SQL Server}};SERVER={server_name};DATABASE={database_name};Trusted_Connection=yes;'
+
+connection = pyodbc.connect(connection_string)
+cursor = connection.cursor()
+
+# Update Klant table
+update_klant_query = """
+UPDATE Klant
+SET
+    KlantNaam = ISNULL(KlantNaam, 'onbekend'),
+    KlantAdres = ISNULL(KlantAdres, 'onbekend'),
+    KlantEmail = ISNULL(KlantEmail, 'onbekend'),
+    KlantTelefoon = ISNULL(KlantTelefoon, 'onbekend'),
+    Fax = ISNULL(Fax, 'onbekend'),
+    KlantPostcode = ISNULL(KlantPostcode, 'onbekend'),
+    KlantStaat = ISNULL(KlantStaat, 'onbekend')
+WHERE
+    KlantNaam IS NULL
+    OR KlantAdres IS NULL
+    OR KlantEmail IS NULL
+    OR KlantTelefoon IS NULL
+    OR Fax IS NULL
+    OR KlantPostcode IS NULL
+    OR KlantStaat IS NULL
+"""
+
+cursor.execute(update_klant_query)
+connection.commit()
+
+# Update Locatie table
+update_locatie_query = """
+UPDATE Locatie
+SET
+    Regio = ISNULL(Regio, 'onbekend'),
+    Stad = ISNULL(Stad, 'onbekend'),
+    Adres = ISNULL(Adres, 'onbekend'),
+    Postcode = ISNULL(Postcode, 'onbekend')
+WHERE
+    Regio IS NULL
+    OR Stad IS NULL
+    OR Adres IS NULL
+    OR Postcode IS NULL
+"""
+
+cursor.execute(update_locatie_query)
+connection.commit()
+
+# Update Product table
+update_product_query = """
+UPDATE Product
+SET
+    Type = ISNULL(Type, 'onbekend'),
+    Quantity = ISNULL(Quantity, 'onbekend'),
+    Color = ISNULL(Color, 'onbekend'),
+    Description = ISNULL(Description, 'onbekend'),
+    Size = ISNULL(Size, 'onbekend')
+WHERE
+    Type IS NULL
+    OR Quantity IS NULL
+    OR Color IS NULL
+    OR Description IS NULL
+    OR Size IS NULL
+"""
+
+cursor.execute(update_product_query)
+connection.commit()
+
+# Update Review table
+update_review_query = """
+UPDATE Review
+SET
+    KlantID = ISNULL(KlantID, 'onbekend')
+WHERE
+    KlantID IS NULL
+"""
+
+cursor.execute(update_review_query)
+connection.commit()
+
+# Close the connection
+cursor.close()
+connection.close()
